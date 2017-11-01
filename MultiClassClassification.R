5989b6fda803e9e8943d636052ebd360904794ba
Vimal <vimal.biochem@gmail.com>
2017-11-01 19:03:06 +0100

    Multiclass classification using xgboost Updated git add .

--- MultiClassClassification.R
+++ MultiClassClassification.R
@@ -53,7 +53,7 @@ library("xgboost")  # the main algorithm
 library("archdata") # for the sample dataset
 library("caret")    # for the confusionmatrix() function (also needs e1071 package)
 library("dplyr")    # for some data preperation
-library(doSNOW)
+#library(doSNOW)
 
 mat.df$class <- as.factor(mat.df$class)
 set.seed(717)
@@ -85,25 +85,23 @@ test_matrix <- xgb.DMatrix(data = test_data, label = test_label)
 # Train Model
 #=================================================================
 
+#cl <- makeCluster(4, type = "SOCK")
+# Register cluster so that caret will know to train in parallel.
+
+#registerDoSNOW(cl)
+
+
+
+
+
 numberOfClasses <- length(unique(mat.df$class)) +1
 xgb_params <- list("objective" = "multi:softprob",
                    "eval_metric" = "mlogloss",
                    "booster" = "gbtree",
                    "num_class" = numberOfClasses)
-nround    <- 100 # number of XGBoost rounds
+nround    <- 50 # number of XGBoost rounds
 cv.nfold  <- 10
 # Fit cv.nfold * cv.nround XGB models and save OOF predictions
-
-#train_matrix <- subset(mat.train, select= -c(class))
-
-cl <- makeCluster(4, type = "SOCK")
-
-# Register cluster so that caret will know to train in parallel.
-registerDoSNOW(cl)
-
-
-
-
 
 cv_model <- xgb.cv(params = xgb_params,
                    data = train_matrix, 
@@ -112,7 +110,7 @@ cv_model <- xgb.cv(params = xgb_params,
                    verbose = FALSE,
                    prediction = TRUE)
 
-stopCluster(cl)
+#stopCluster(cl)
 
 
 OOF_prediction <- data.frame(cv_model$pred) %>%
@@ -125,6 +123,67 @@ head(OOF_prediction)
 confusionMatrix(factor(OOF_prediction$label), 
                 factor(paste0('group', OOF_prediction$max_prob -1)),
                 mode = "everything")
+
+
+
+
+
+################ With Grid search
+
+# set up the cross-validated hyper-parameter search
+xgb_grid_1 = expand.grid(
+  nrounds = 100,
+  eta = c(0.01, 0.001, 0.0001),
+  max_depth = c(2, 4, 6, 8, 10),
+  gamma = 1
+)
+# pack the training control parameters
+xgb_trcontrol_1 = trainControl(
+  method = "cv",
+  number = 3, 
+  summaryFunction = multiClassSummary,
+  allowParallel = TRUE
+)
+
+xgbGrid = expand.grid(
+  nrounds = 2, 
+  max_depth = c(5, 10, 15), 
+  eta = c(0.01, 0.001, 0.0001), 
+  gamma = c(1, 2, 3), 
+  colsample_bytree = c(0.4, 0.7, 1.0), 
+  min_child_weight = c(0.5, 1, 1.5),
+  subsample=c(0.2,0.2,0.2,0.2,0.2)
+)
+
+
+
+
+
+
+# train the model for each parameter combination in the grid,
+#   using CV to evaluate
+xgb_train_1 = train(
+  x = train_data,
+  y = train_label,
+  trControl = xgb_trcontrol_1,
+  tuneGrid = xgbGrid,
+  method = "xgbTree"
+)
+# scatter plot of the AUC against max_depth and eta
+ggplot(xgb_train_1$results, aes(x = as.factor(eta), y = max_depth, size = ROC, color = ROC)) +
+  geom_point() +
+  theme_bw() +
+  scale_size_continuous(guide = "none")
+
+
+
+
+
+
+
+
+
+
 
 
 
